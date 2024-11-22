8bb2eff8debc250331ef3e35b7fce4ad5e574580
Kennedysenyo <kennedysenyo@gmail.com>
2024-11-22 01:56:52 +0000

    Add put route functionlity

--- index.js
+++ index.js
@@ -10,33 +10,82 @@ const url = "https://bored-api.appbrewery.com";
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(`${url}/random`);
+    const result = response.data;
+    res.render("index.ejs", {
+      response: result,
+    })
+  } catch (error) {
+    console.error("Fialed to make request:", error.message);
+    res.render("index.ejs", {
+      error: "Ooops! Server down, refresh.",
+    })
+  }
 });
 
 app.post("/search", async (req, res) => {
-  console.log(req.body)
-  let response = ""
   const type = req.body.type;
   const participants = req.body.participants;
 
   if (type === "random" && participants == "random") {
-    response = await axios.get(`${url}/random`);
+    try {
+      const response = await axios.get(`${url}/random`);
+      const result = response.data;
 
-    console.log(response.data);
-    res.render("index.ejs", {
-      response: response.data,
-    });
+      console.log(result);
+      res.render("index.ejs", {
+        response: result,
+      });
+
+    } catch(error) {
+      console.error("Fialed to make request:", error.message);
+      res.render("index.ejs", {
+        error: "Ooops! Server down, refresh.",
+      });
+    }
+  }
+  else if (type !== "random" && participants === "random") {
+    try {
+      const response = await axios.get(`${url}/filter?type=${type}`);
+      const result = response.data;
+
+      res.render("index.ejs", {
+        response: result[Math.floor(Math.random() * result.length)],
+      });
+    } catch (error) {
+      console.error("Fialed to make request:", error.message);
+      res.render("index.ejs", {
+        error: "Ooops! Server down, refresh.",
+      });
+    }
+  } 
+  else if (type === "random" && participants !== "random") {
+    const response = await axios.get(`${url}/filter?participants=${participants}`);
+      const result = response.data;
+
+      res.render("index.ejs", {
+        response: result[Math.floor(Math.random() * result.length)],
+      });
   }
   else {
-    console.log(`${url}/filter?type=${type}&participants=${participants}`);
-    response = await axios.get(`${url}/filter?type=${type}&participants=${participants}`);
 
- 
-    console.log(response.data);
-    res.render("index.ejs", {
-      response: response.data,
-    });
+    try {
+      const response = await axios.get(`${url}/filter?type=${type}&participants=${participants}`);
+    
+      const result = response.data;
+      res.render("index.ejs", {
+        response: result[Math.floor(Math.random() * result.length)],
+      });
+  
+    } catch (error) {
+      console.error("Fialed to make request:", error.message);
+      res.render("index.ejs", {
+        error: "No activities meet your search criteria",
+      });
+    }
+   
   }
   
 });  

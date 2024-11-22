b668596e3015ba82f749d43bc702a908b7beefe8
Kennedysenyo <kennedysenyo@gmail.com>
2024-11-22 02:17:12 +0000

    Add modifications and error handling

--- index.js
+++ index.js
@@ -14,76 +14,117 @@ app.get("/", async (req, res) => {
   try {
     const response = await axios.get(`${url}/random`);
     const result = response.data;
+
     res.render("index.ejs", {
       response: result,
-    })
+    });
   } catch (error) {
     console.error("Fialed to make request:", error.message);
+
     res.render("index.ejs", {
       error: "Ooops! Server down, refresh.",
-    })
+    });
   }
 });
 
 app.post("/search", async (req, res) => {
   const type = req.body.type;
   const participants = req.body.participants;
+  let statusCode = "";
 
   if (type === "random" && participants == "random") {
     try {
       const response = await axios.get(`${url}/random`);
       const result = response.data;
+      statusCode = response.status;
 
-      console.log(result);
       res.render("index.ejs", {
         response: result,
       });
 
     } catch(error) {
       console.error("Fialed to make request:", error.message);
-      res.render("index.ejs", {
-        error: "Ooops! Server down, refresh.",
-      });
+
+      if (statusCode === 429) {
+        res.render("index.ejs", {
+          error: "Ooops! Server down, refresh.",
+        });
+      } else {
+        res.render("index.ejs", {
+          error: "No activities meet your search criteria",
+        });
+      }
     }
   }
   else if (type !== "random" && participants === "random") {
     try {
       const response = await axios.get(`${url}/filter?type=${type}`);
       const result = response.data;
+      statusCode = response.status;
 
       res.render("index.ejs", {
         response: result[Math.floor(Math.random() * result.length)],
       });
     } catch (error) {
       console.error("Fialed to make request:", error.message);
-      res.render("index.ejs", {
-        error: "Ooops! Server down, refresh.",
-      });
+
+      if (statusCode === 429) {
+        res.render("index.ejs", {
+          error: "Ooops! Server down, refresh.",
+        });
+      } else {
+        res.render("index.ejs", {
+          error: "No activities meet your search criteria",
+        });
+      }
     }
   } 
   else if (type === "random" && participants !== "random") {
-    const response = await axios.get(`${url}/filter?participants=${participants}`);
+    try {
+      const response = await axios.get(`${url}/filter?participants=${participants}`);
       const result = response.data;
+      statusCode = response.status;
 
       res.render("index.ejs", {
         response: result[Math.floor(Math.random() * result.length)],
       });
+    } catch (error) {
+      console.error("Fialed to make request:", error.message);
+
+      if (statusCode === 429) {
+        res.render("index.ejs", {
+          error: "Ooops! Server down, refresh.",
+        });
+      } else {
+        res.render("index.ejs", {
+          error: "No activities meet your search criteria",
+        });
+      }
+    }
   }
   else {
 
     try {
       const response = await axios.get(`${url}/filter?type=${type}&participants=${participants}`);
-    
       const result = response.data;
+      statusCode = response.status;
+
       res.render("index.ejs", {
         response: result[Math.floor(Math.random() * result.length)],
       });
   
     } catch (error) {
       console.error("Fialed to make request:", error.message);
-      res.render("index.ejs", {
-        error: "No activities meet your search criteria",
-      });
+
+      if (statusCode === 429) {
+        res.render("index.ejs", {
+          error: "Ooops! Server down, refresh.",
+        });
+      } else {
+        res.render("index.ejs", {
+          error: "No activities meet your search criteria",
+        });
+      }
     }
    
   }
